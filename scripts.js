f08016aa8428c82bce5868c4da1dafbfc23ad799
Hauly90 <j.macicek@centrum.cz>
2020-04-26 21:22:57 +0200

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,26 @@
+let myArray = ["Jakub", "Věra", "Jan", "Anakin", "Ředitel"];
+
+$("ul").append("<li>Jakub</li>");
+$("ul").append("<li>Věra</li>");
+$("ul").append("<li>Jan</li>");
+$("ul").append("<li>Anakin</li>");
+$("ul").append("<li>Ředitel</li>");
+
+//$("ul").append($myArray);
+//$("ul").append(document.createTextNode("<li>Jakub</li>"));
+
+/*$("#box-container").append("<div class="box"></div>");
+$(".box:last-child").css("background", myArray[0]);
+$("#box-container").append("<div class="box"></div>");
+$(".box:last-child").css("background", myArray[1]);
+$("#box-container").append("<div class="box"></div>");
+$(".box:last-child").css("background", myArray[2]);
+$("#box-container").append("<div class="box"></div>");
+$(".box:last-child").css("background", myArray[3]);
+$("#box-container").append("<div class="box"></div>");
+$(".box:last-child").css("background", myArray[4]);
+
+myArray.forEach((myArray) => {
+    $("#box-container").append("<div class="box"></div>");
+    $(".box:last-child").css("background", myArray);
+  });*/
